1c3e23261f67031b9da49c223e9802313a6be03c
Nataliia Ostashevska <fistashka116@gmail.com>
2021-10-31 01:32:02 +0300

    init commit

--- src/com/pb/ostashevska/hw4/Anagram.java
+++ src/com/pb/ostashevska/hw4/Anagram.java
@@ -5,25 +5,31 @@ import java.util.Arrays;
 public class Anagram {
 
     static boolean procedure(String value1, String value2) {
+        value1 = value1.toLowerCase();
+        value2 = value2.toLowerCase();
+
         String[] value1Parts = value1.split(" ");
         for (int i=0; i < value1Parts.length; i++) {
             char[] charArray = value1Parts[i].toCharArray();
             Arrays.sort(charArray);
             String sort1 = new String(charArray);
+            System.out.println(sort1);
         }
-        String Value1 = String.join("", value1Parts);
+        String Value1 = String.join(" ",value1Parts);
+        System.out.println(Value1);
+
         String[] value2Parts = value2.split(" ");
         for (int i=0; i < value2Parts.length; i++) {
-            char[] charArray = value1Parts[i].toCharArray();
+            char[] charArray = value2Parts[i].toCharArray();
             Arrays.sort(charArray);
             String sort2 = new String(charArray);
+            System.out.println(sort2);
         }
-        String Value2 = String.join("", value2Parts);
-        boolean res = Value1.equalsIgnoreCase(Value2);
-        if (res==true){
-            System.out.println("рядки є анаграмами");
-        } else { System.out.println("рядки не є анаграмами");}
-        return res;
+        String Value2 = String.join(" ", value2Parts);
+        System.out.println(Value2);
+
+        return value1.equals(value2);
+
     }
     public static void main(String[] args) {
         System.out.println("введіть текст");
@@ -32,5 +38,11 @@ public class Anagram {
         String value2 = in.next();
 
         System.out.println(procedure(value1, value2));
+
+        boolean res = value1.equals(value2);
+        if (res==true){
+            System.out.println("рядки є анаграмами");
+        } else { System.out.println("рядки не є анаграмами");}
+
     }
 }
--- src/com/pb/ostashevska/hw4/CapitalLetter.java
+++ src/com/pb/ostashevska/hw4/CapitalLetter.java
@@ -1,15 +1,16 @@
 package com.pb.ostashevska.hw4;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class CapitalLetter {
 
    static String procedure(String value) {
-       String[] valueParts = value.split(" ");
-       for (int i=0; i < valueParts.length; i++) {
+      String[] valueParts = value.split(" ");
+
+           for (int i=0; i < valueParts.length; i++) {
+
            valueParts [i] = valueParts[i].substring(0, 1).toUpperCase() + valueParts[i].substring(1).toLowerCase();
        }
-       String Value = String.join("", valueParts);
+       String Value = String.join(" ", valueParts);
        return Value;
     }
     public static void main(String[] args) {
