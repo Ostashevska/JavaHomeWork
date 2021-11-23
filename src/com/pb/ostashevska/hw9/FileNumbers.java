ee8c0596594ecc29cba98f1c4d3e63906b03d1b0
Nataliia Ostashevska <fistashka116@gmail.com>
2021-11-23 23:48:39 +0200

    init commit

---
+++ src/com/pb/ostashevska/hw9/FileNumbers.java
@@ -0,0 +1,18 @@
+package com.pb.ostashevska.hw9;
+
+public class FileNumbers {
+
+    public  void createNumbersFile(){
+
+    }
+
+    public void createOddNumbersFile(){
+
+    }
+
+    public  void  mane(String[] args){
+
+
+    }
+
+}
