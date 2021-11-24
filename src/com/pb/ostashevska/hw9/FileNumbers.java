99b07cbb1e508bce3db705e8b56143ea99228f45
Nataliia Ostashevska <fistashka116@gmail.com>
2021-11-24 04:20:26 +0200

    init commit

--- src/com/pb/ostashevska/hw9/FileNumbers.java
+++ src/com/pb/ostashevska/hw9/FileNumbers.java
@@ -1,16 +1,76 @@
 package com.pb.ostashevska.hw9;
 
+import java.io.*;
+import java.util.Random;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.nio.charset.StandardCharsets;
+
+
 public class FileNumbers {
 
-    public  void createNumbersFile(){
+    private static final Logger LOGGER = Logger.getLogger(FileNumbers.class.getName());
+
+    public  void createNumbersFile() throws Exception {
+        Path testFile1 = Files.createFile(Paths.get("file/numbers.txt"));
+        System.out.println(Files.exists(Paths.get("file/numbers.txt")));
+        Path path = Paths.get("file/numbers.txt");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+           for (int i=0; i<=10; i++) {
+               int[] array = new int[10];
+               final Random random = new Random();
+               for (int j=0; j<=array.length; j++ ) {
+                   array[j]= random.nextInt();
+
+                   final String oneLine = Integer.toString(array[j]);
+                   writer.write(oneLine);
+               }
+            writer.newLine();
+           }
+        } catch (Exception ex) {
+            System.out.println("Error with file write: " + ex);
+        }
+        System.out.println("Write to file \"" + path.toAbsolutePath() + "\" done!");
+    }
+
+    public void createOddNumbersFile() throws IOException {
+        LOGGER.entering(getClass().getName(), "createOddNumbersFile");
+
+        Path testFile1 = Files.createFile(Paths.get("file/odd-numbers.txt"));
+        System.out.println(Files.exists(Paths.get("file/odd-numbers.txt")));
+
+        Path path = Paths.get("file/numbers.txt");
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (String s : lines) {
+                int i = Integer.parseInt(s.trim());
+                if (i % 2 == 1) {
+                    i = 0;
+                }
+                writer.write(i);
+                writer.newLine();
+            }
+        } catch (Exception ex) {
+            System.out.println("Error with file write: " + ex);
+        }
+        System.out.println("Write to file \"" + path.toAbsolutePath() + "\" done!");
 
     }
 
-    public void createOddNumbersFile(){
+    public static void main(String[] args) throws Exception {
 
-    }
+        System.setProperty("java.util.logging.config.file", "out/production/JavaHomeWork/com/pb/ostashevska/hw9/Logger");
+        LogManager.getLogManager().readConfiguration();
 
-    public  void  mane(String[] args){
+        FileNumbers examples = new FileNumbers();
+        examples.createNumbersFile();
+        examples.createOddNumbersFile();
 
 
     }
