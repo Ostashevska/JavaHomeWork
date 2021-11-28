682095ae7a350e67cc2866c2cb86f198937c4e27
Nataliia Ostashevska <fistashka116@gmail.com>
2021-11-28 23:53:25 +0200

    init commit

--- src/com/pb/ostashevska/hw8/Auth.java
+++ src/com/pb/ostashevska/hw8/Auth.java
@@ -7,31 +7,21 @@ public class Auth {
     public String password = "Pa55w0rd";
 
 
-    public void signUp(@NotNull String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+    /*public void signUp(@NotNull String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
 
-        if (login.length() < 20 && login.length() > 5) {
-            if (!login.matches("[a-zA-Z0-9_]")) {
+            if (login.matches("[a-zA-Z0-9_],{5,20}")) {
+                this.login = login;
+            } else {
                 System.out.println("Логін не відповідає вимогам: логін може містити лише латинські літери та цифри");
                 throw new WrongLoginException();
-            } else {
-                this.login = login;
             }
-        } else {
-            System.out.println("Логін не відповідає вимогам: довжина логіну повинна бути більше 5-ти та менше 20-ти символів");
-            throw new WrongLoginException();
-        }
 
-        if (password.length() > 5) {
-            if (password.matches("[a-zA-Z0-9_]") && password ==confirmPassword) {
+            if (password.matches("[a-zA-Z0-9_],{5, }") && password ==confirmPassword) {
                 this.password = password;
             } else {
                 System.out.println("Пароль не відповідає вимогам: може містити лише латинські літери, цифри та символ підкреслення");
                 throw new WrongPasswordException();
             }
-        } else {
-            System.out.println("Пароль не відповідає вимогам: довжина паролю повинна бути більше 5-ти символів");
-            throw new WrongPasswordException();
-        }
     }
 
     public void signIn(String login, String password) throws WrongLoginException {
@@ -41,6 +31,33 @@ public class Auth {
         } else {
             System.out.println("Невірний логін");
             throw new WrongLoginException();
+        }
+    }
+}*/
+
+    public void signUp(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (login == null || !login.matches("[a-zA-Z0-9]{5,20}")) {
+            throw new WrongLoginException("Login length must be between 5 and 20 symbols, contains only letters and numbers");
+        }
+        if (password == null || !password.matches("[a-zA-Z0-9_]{5,}")) {
+            throw new WrongPasswordException("Password length must be >= 5 symbols, contains only letters, numbers and symbol \"_\"");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Password not confirmed");
+        }
+        this.login = login;
+        this.password = password;
+    }
+
+    /**
+     * Login as registered user
+     */
+    public void signIn(String login, String password) throws WrongLoginException {
+        if (!this.login.equals(login)) {
+            throw new WrongLoginException("User not found");
+        }
+        if (!this.password.equals(password)) {
+            throw new WrongLoginException("User not found");
         }
     }
 }
