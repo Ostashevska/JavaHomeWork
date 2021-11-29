f33b6c330047defd3f084504b217ab010c0d6974
Nataliia Ostashevska <fistashka116@gmail.com>
2021-11-29 03:40:25 +0200

    init commit

--- src/com/pb/ostashevska/hw10/Main.java
+++ src/com/pb/ostashevska/hw10/Main.java
@@ -3,7 +3,7 @@ package com.pb.ostashevska.hw10;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-       NumBox<Float> num1= new NumBox<>(5);
+      /* NumBox<Float> num1= new NumBox<>(5);
        num1.add(0, 2F);
         num1.add(1, 2.5F);
         num1.add(2, 5F);
@@ -14,7 +14,20 @@ public class Main {
         num1.average();
         num1.length ();
         num1.get(3);
-        num1.max();
+        num1.max();*/
+
+        NumBox<Integer> num2= new NumBox<>(5);
+        num2.add(0, 2);
+        num2.add(1, (int) 2.5);
+        num2.add(2, 5);
+        num2.add(3, 8);
+        num2.add(4, (int) 7.1);
+
+        num2.sum ();
+        num2.average();
+        num2.length ();
+        num2.get(3);
+        num2.max();
 
     }
 }
